98d2989e04702fae3432ff9f2947cd280f7c757e
Sanjaykumar <sanjaykumar679593@gmail.com>
2025-03-02 20:29:24 +0530

    padding changed from 4 to 8 in parent container

--- client/src/pages/student/CourseDetails.jsx
+++ client/src/pages/student/CourseDetails.jsx
@@ -90,7 +90,7 @@ const CourseDetails = () => {
 
   return courseData ? (
     <>
-      <div className="flex md:flex-row flex-col-reverse gap-10 relative items-start justify-between md:px-36 px-8 md:pt-30 pt-20 text-left">
+      <div className="flex md:flex-row flex-col-reverse gap-10 relative items-start justify-between px-4 md:px-36 md:pt-30 pt-20 text-left">
         <div className="absolute top-0 left-0 w-full h-full -z-1 bg-gradient-to-b from-cyan-100/70"></div>
 
         {/* left column */}
